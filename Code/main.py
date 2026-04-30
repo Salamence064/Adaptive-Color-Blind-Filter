43fef612073c089a464bff6e1811d3fd22089857
AsianPantsMan <casualcaseytime@gmail.com>
2026-04-29 22:12:22 -0500

    create image input arg

--- Code/main.py
+++ Code/main.py
@@ -1,1 +1,15 @@
-print("Aloha!")
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("image_path", help="Path to the input image")
+    args = parser.parse_args()
+
+    image = plt.imread(args.image_path)
+
+    outputDir = '../Outputs/'
+
+    plt.imsave(os.path.join(outputDir, "image.png"), image)
