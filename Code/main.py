be137dfdef42b277fb2a180b1065edff36165598
AsianPantsMan <casualcaseytime@gmail.com>
2026-04-30 14:51:04 -0500

    create transform matrix application function

--- Code/main.py
+++ Code/main.py
@@ -1,15 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2
 import os
 import argparse
 
+
+
+def apply_transform(image, matrix):
+    # apply per-pixel transform: result[x,y,:] = kernel @ image[x,y,:]
+    out = np.tensordot(image, matrix.T, axes=([2], [0]))  #HxWx3
+    out = np.clip(out, 0, 255)
+
+    return out
+
 if __name__ == "__main__":
+    outputDir = '../Outputs/'
+
+    #use format 'python3 main.py {pathname}' to run code
     parser = argparse.ArgumentParser()
     parser.add_argument("image_path", help="Path to the input image")
     args = parser.parse_args()
 
-    image = plt.imread(args.image_path)
+    image = cv2.imread(args.image_path)
 
-    outputDir = '../Outputs/'
+    #BGR to RGB
+    image_rgb = image[:, :, ::-1]
 
-    plt.imsave(os.path.join(outputDir, "image.png"), image)
+    plt.imsave(os.path.join(outputDir, "image.png"), image_rgb)
