d53427b68b569bc82e3b5830def2fde9ae224522
Salamence064 <thomas.ducote64@gmail.com>
2026-04-30 21:24:37 -0500

    Everything worky works

--- Code/main.py
+++ Code/main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import cv2
 import os
 import argparse
+import random
 
 
 #define daltonization transform matrices
@@ -25,7 +26,7 @@ DALTONIZATION_MATRICES = {
     ])
 }
 
-GRADIENT_IMAGE_RGB = cv2.imread("Images/gradient.jpg")
+GRADIENT_IMAGE_RGB = cv2.imread("../Images/gradient.jpg")
 GRADIENT_IMAGE_RGB = GRADIENT_IMAGE_RGB[:, :, ::-1] # BGR to RGB
 
 
@@ -34,7 +35,7 @@ def weigh_matrices(matrices: dict[str, np.ndarray], weights: dict[str, float]) -
 
     w = np.array([weights[n] for n in names], dtype=np.float64)
 
-    if w.sum() != 1:
+    if w.sum() == 0.0:
         return np.array([
             [1, 0, 0],
             [0, 1, 0],
@@ -53,10 +54,18 @@ def apply_transform(image, matrix):
     return out
 
 
+def clamp(n, a, b) -> float:
+    return max(a, min(n, b))
+
 def normalize(w: tuple) -> tuple:
+    true_vals = list(w)
+
+    for i in range(len(true_vals)):
+        true_vals[i] = clamp(true_vals[i], 0.0, 1.0)
+
     arr = []
 
-    for i in w: arr.append(i / sum(w))
+    for val in true_vals: arr.append(val / sum(true_vals))
     res: tuple = tuple(arr)
 
     return res
@@ -70,15 +79,26 @@ def average(n1, n2):
 # If old = new, pass in the other two filters as parameters, too
 # Outputs a tuple of tuples with the weights for the two new filters ((r, g, b), (r, g, b)) 
 def apply_new_filters(weights: tuple) -> tuple:
+    old = weights[0]
     new = weights[1]
 
     f1 = weights[2]
     f2 = weights[3]
     f3 = weights[4]
 
-    f1_avg = (average(f1[0], new[0]), average(f1[1], new[1]), average(f1[2], new[2]))
-    f2_avg = (average(f2[0], new[0]), average(f2[1], new[1]), average(f2[2], new[2]))
-    f3_avg = (average(f3[0], new[0]), average(f3[1], new[1]), average(f3[2], new[2]))
+    ran_r = 0.0
+    ran_g = 0.0
+    ran_b = 0.0
+
+    # add variance if it is the same one otherwise we can get stuck before it reaches a natural point for this to happen
+    if old[0] == new[0] and old[1] == new[1] and old[2] == new[2]:
+        ran_r = random.uniform(-0.1, 0.1)
+        ran_g = random.uniform(-0.1, 0.1)
+        ran_b = random.uniform(-0.1, 0.1)
+
+    f1_avg = (average(f1[0], new[0]) + ran_r, average(f1[1], new[1]) + ran_g, average(f1[2], new[2]) + ran_b)
+    f2_avg = (average(f2[0], new[0]) + ran_r, average(f2[1], new[1]) + ran_g, average(f2[2], new[2]) + ran_b)
+    f3_avg = (average(f3[0], new[0]) + ran_r, average(f3[1], new[1]) + ran_g, average(f3[2], new[2]) + ran_b)
 
     return (normalize(f1_avg), normalize(f2_avg), normalize(f3_avg))
 
@@ -86,7 +106,7 @@ def apply_new_filters(weights: tuple) -> tuple:
 # Display three images each with a different filter applied for the user to choose between
 # This takes the weights in for the two filters applied and for the original ((r, g, b), (r, g, b), (r, g, b))
 # Returns the figure ID
-def display_filters(weights: tuple):
+def display_filters(weights: tuple) -> None:
     # format the weights appropriately
     w1 = {
         "protanopia": weights[0][0],
@@ -126,6 +146,8 @@ def display_filters(weights: tuple):
     ims = [im1, im2, im3, im4]
 
    
+    plt.close('all')
+
     fig, axes = plt.subplots(1, 4, figsize=(12, 4))
 
     for i, (ax, img) in enumerate(zip(axes, ims), start=1):
@@ -140,21 +162,23 @@ def display_filters(weights: tuple):
 
     plt.tight_layout()
     plt.subplots_adjust(bottom=0.4)
-    plt.show(block=False)
-
-    return fig.number
+    
+    fig.canvas.draw_idle()
+    plt.pause(0.05)
 
 
 # Loops until the user is satisfied with the filter
 # Outputs a tuple
 def find_optimal_filter() -> tuple:
+    plt.ion()
+
     curr_filter: tuple = (0.0, 0.0, 0.0)
     filter1: tuple = (1.0, 0.0, 0.0)
     filter2: tuple = (0.0, 1.0, 0.0)
     filter3: tuple = (0.0, 0.0, 1.0)
 
     while True:
-        fig_id = display_filters((curr_filter, filter1, filter2, filter3))
+        display_filters((curr_filter, filter1, filter2, filter3))
 
         selected_img = input("Which image do you like most (1-4)? ")
         if selected_img == 'quit' or selected_img == 'q':
@@ -180,56 +204,66 @@ def find_optimal_filter() -> tuple:
                 filter1 = (1.0, 0.0, 0.0)
                 filter2 = (0.0, 1.0, 0.0)
                 filter3 = (0.0, 0.0, 1.0)
+                break
         
         og_filter = curr_filter
 
         if selected_img == '2': curr_filter = filter1
         elif selected_img == '3': curr_filter = filter2
         elif selected_img == '4': curr_filter = filter3
-        
-        if plt.fignum_exists(fig_id): plt.show()
-        
-        filter1, filter2, filter3 = apply_new_filters((og_filter, curr_filter, filter1, filter2, filter3))
+
+        if selected_img != 'restart' and selected_img != 'r':       
+            filter1, filter2, filter3 = apply_new_filters((og_filter, curr_filter, filter1, filter2, filter3))
 
     return curr_filter
 
 
 if __name__ == "__main__":
-    final_filter = find_optimal_filter()
-    print(final_filter)
+    outputDir = '../Output'
+    imagesDir = "../Images/"
+    os.makedirs(outputDir, exist_ok=True)
 
-    # outputDir = '../Outputs/'
-    # os.makedirs(outputDir, exist_ok=True)
+    #use format 'python3 main.py {pathname}' to run code
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", help="Path to the input image")
+    args = parser.parse_args()
 
-    # #use format 'python3 main.py {pathname}' to run code
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("image_path", help="Path to the input image")
-    # args = parser.parse_args()
+    if not args.i:
+        final_filter = find_optimal_filter()
 
-    # image = cv2.imread(args.image_path)
+        with open(f"{outputDir}/filter.txt", 'w') as out:
+            out.write(str(final_filter))
+            print("Saved filter to file.")
+            print("Rerun with a filepath to apply it!")
+    
+    else:
+        if not os.path.exists(outputDir + "/filter.txt"):
+            print("Run without a filepath first to get a filter value!")
+            exit(0)
 
-    # #select matrix to apply
-    # # matrix_name = "protanopia"
-    # # matrix_name = "deuteranopia"
-    # matrix_name = "tritanopia"
+        image = cv2.imread(imagesDir + args.i)
 
-    # weights = {
-    #     "protanopia": 0.6,
-    #     "deuteranopia": 0.3,
-    #     "tritanopia": 0.1,
-    # }
+        with open(outputDir + '/filter.txt', 'r') as f:
+            weighty_weights = f.read().strip()[1:-2].split(', ')
+            for i in range(len(weighty_weights)): weighty_weights[i] = float(weighty_weights[i])
 
-    # applied_matrix = weigh_matrices(DALTONIZATION_MATRICES, weights)
+        weights = {
+            "protanopia": weighty_weights[0],
+            "deuteranopia": weighty_weights[1],
+            "tritanopia": weighty_weights[2],
+        }
 
-    # #BGR to RGB
-    # image_rgb = image[:, :, ::-1]
+        applied_matrix = weigh_matrices(DALTONIZATION_MATRICES, weights)
 
-    # transformed_image = apply_transform(image_rgb, applied_matrix)
+        #BGR to RGB
+        image_rgb = image[:, :, ::-1]
 
-    # #save image
-    # p_weight = weights.get("protanopia", 0.0)
-    # d_weight = weights.get("deuteranopia", 0.0)
-    # t_weight = weights.get("tritanopia", 0.0)
-    # weight_tag = f"{p_weight}_{d_weight}_{t_weight}"
-    # input_name = os.path.splitext(os.path.basename(args.image_path))[0]
-    # plt.imsave(os.path.join(outputDir, f"{weight_tag}_{input_name}.png"), transformed_image)
+        transformed_image = apply_transform(image_rgb, applied_matrix)
+
+        #save image
+        p_weight = weights.get("protanopia", 0.0)
+        d_weight = weights.get("deuteranopia", 0.0)
+        t_weight = weights.get("tritanopia", 0.0)
+        weight_tag = f"{p_weight}_{d_weight}_{t_weight}"
+        input_name = os.path.splitext(os.path.basename(args.i))[0]
+        plt.imsave(os.path.join(outputDir, f"{weight_tag}_{input_name}.png"), transformed_image)
